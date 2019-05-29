3a316c36f3ef24efcb0fef466df52fed30aceec9
Wessel van der Veen <w.vanderveen@thesio.eu>
2019-05-29 10:43:29 +0200

    upgraded to 0.12

--- variables.tf
+++ variables.tf
@@ -1,71 +1,72 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Solution name, e.g. 'app' or 'jenkins'"
 }
 
 variable "enabled" {
-  type        = "string"
+  type        = string
   default     = "true"
   description = "Set to false to prevent the module from creating any resources"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
 }
 
 variable "additional_tag_map" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags for appending to each tag map"
 }
 
 variable "context" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Default context to use for passing state between label invocations"
 }
 
 variable "label_order" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "The naming order of the id output and Name tag"
 }
 
 variable "regex_replace_chars" {
-  type        = "string"
+  type        = string
   default     = "/[^a-zA-Z0-9-]/"
   description = "Regex to replace chars with empty string in `namespace`, `environment`, `stage` and `name`. By default only hyphens, letters and digits are allowed, all other chars are removed"
 }
+
